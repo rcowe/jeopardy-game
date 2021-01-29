c84e7200e8cbff7ce4d281b2284ed9d12308461e
rcbdelarosa <rcb.delarosa@gmail.com>
2021-01-29 02:47:28 -0600

    FINALLY GOT API WORKING

--- src/components/App.js
+++ src/components/App.js
@@ -1,76 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import JeopardyHome from './JeopardyHome';
 // import axios from 'axios';
-// import JeopardyHome from './JeopardyHome';
-
-// export default function App() {
-// 	const [currentQuestion, updateQuestions] = useState([]);
-
-// 	useEffect(() => {
-// 		axios.get('http://jservice.io/api/random').then(
-// 			res => {
-// 				updateQuestions(res.data.question.map(jquestion => jquestion.question));
-// 			},
-// 			error => {
-// 				console.log(error);
-// 			}
-// 		);
-// 	}, []);
-
-// 	return (
-// 		<div>
-// 			<h1>Jeopardy Trivia Game!</h1>
-// 			{currentQuestion.map(question => {
-// 				return (
-// 					<JeopardyHome
-// 						key={`${question.id}-${question.airdate}`}
-// 						question={question}
-// 					/>
-// 				);
-// 			})}
-// 		</div>
-// 	);
-// }
 
 export default function App(props) {
-	const [query, updateQuery] = useState({
-		baseURL: 'http://jservice.io/api/random',
-		random: '',
-		score: 0
-	});
+	// const [query, updateQuery] = useState({
+	// 	baseURL: 'http://jservice.io/api/random',
+	// 	random: '',
+	// 	score: 0
+	// });
 
 	const [currentQuestion, updateQuestion] = useState({});
 	useEffect(() => {
-		async () => {
-			if (query.random) {
-				try {
-					const response = await fetch(query.random);
-					const data = await response.json();
-					await updateQuestion(data);
-				} catch (error) {
-					console.error(error);
-				}
+		(async () => {
+			try {
+				const response = await fetch('http://jservice.io/api/random');
+				const data = await response.json();
+				updateQuestion(data);
+			} catch (error) {
+				console.error(error);
 			}
-		};
+		})();
 	}, []);
+
 	return (
-		<div>
+		<div className="Page-wrapper">
 			<h1>Jeopardy Trivia Game!</h1>
 			<div>
-				<h1>Question</h1>
-				{/* {Object.keys(currentQuestion).length ? (
+				{Object.keys(currentQuestion).length ? (
 					<JeopardyHome currentQuestion={currentQuestion} />
 				) : (
 					''
-				)} */}
-				{currentQuestion.map(questions => {
-					return (
-						<JeopardyHome
-							key={`${questions.id}${questions.airdate}`}
-							questions={q}
-						/>
-					);
-				})}
+				)}
 			</div>
 		</div>
 	);
--- src/components/JeopardyHome.js
+++ src/components/JeopardyHome.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
-export default function JeopardyHome({ q }) {
+export default function JeopardyHome({ currentQuestion }) {
 	return (
-		<div key={`${questions.id}${questions.airdate}`}>
-			<h1>{q.question}</h1>
-			<h1>{q.airdate}</h1>
+		<div className={'board'}>
+			<h2>Score</h2>
+			<button type="button">Add Points</button>
+			<button type="button">Lower Points</button>
+			<div className={'questions-card'}>
+				<div className="category">
+					<h2>CATEGORY: {currentQuestion[0].category.title}</h2>
+				</div>
+				<h2> QUESTION: {currentQuestion[0].question}</h2>
+				<div className={'hintBox'}>
+					<h2>HINT: </h2>
+				</div>
+				<div className={'answers'}>
+					<h2>ANSWER: {currentQuestion[0].answer}</h2>
+				</div>
+			</div>
 		</div>
 	);
 }
