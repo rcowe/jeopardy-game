dee2742d492b032f1b60bf9db7e03aba92994c5c
rcbdelarosa <rcb.delarosa@gmail.com>
2021-01-31 21:18:04 -0600

    got the update question feature to work

--- src/components/App.js
+++ src/components/App.js
@@ -3,42 +3,55 @@ import JeopardyHome from './JeopardyHome';
 // import axios from 'axios';
 
 export default function App(props) {
+	const [query, updateQuery] = useState({
+		baseURL: 'http://jservice.io/api/random',
+		random: '',
+		curentScore: 0
+	});
 	const [currentQuestion, updateQuestion] = useState({});
+
+	// const apiURL = 'http://jservice.io/api/random';
+
 	useEffect(() => {
 		(async () => {
 			try {
-				const response = await fetch('http://jservice.io/api/random');
+				const response = await fetch(query.baseURL);
 				const data = await response.json();
 				updateQuestion(data);
+				// console.log(updateQuestion(data));
 			} catch (error) {
 				console.error(error);
 			}
 		})();
-	}, []);
+	}, [query]);
 
-	// const handleChange = event => {
-	// 	updateQuery({
-	// 		...query,
-	// 		...{
-	// 			[event.target.id]: event.target.value
-	// 		}
-	// 	});
-	// };
+	const handleChange = event => {
+		updateQuery({
+			...currentQuestion,
+			...{
+				[event.target.id]: event.target.value
+			}
+		});
+	};
 
-	// const handleSubmit = event => {
-	// 	event.preventDefault();
-	// 	updateQuestion({
-	// 		...currentQuestion,
-
-	// 	})
-	// }
+	const handleSubmit = event => {
+		event.preventDefault();
+		updateQuery({
+			...query,
+			random: query.baseURL
+		});
+	};
 
 	return (
-		<div className="Page-wrapper">
+		<div className="app-page">
 			<h1>Welcome To The JeoParty!</h1>
 			<div>
 				{Object.keys(currentQuestion).length ? (
-					<JeopardyHome currentQuestion={currentQuestion} />
+					<JeopardyHome
+						currentQuestion={currentQuestion}
+						handleChange={handleChange}
+						handleSubmit={handleSubmit}
+					/>
 				) : (
 					''
 				)}
--- src/components/JeopardyHome.js
+++ src/components/JeopardyHome.js
@@ -1,22 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function JeopardyHome({ currentQuestion }) {
+export default function JeopardyHome(props) {
+	const [score, updateScore] = useState(0);
+
+	const incrementScore = () =>
+		updateScore(score + props.currentQuestion[0].value);
+
+	const decrementScore = () =>
+		score === 0
+			? updateScore(0)
+			: updateScore(score - props.currentQuestion[0].value);
+
 	return (
-		<div className={'container'}>
+		<div className="container">
 			<h2>Yes, it is in fact a Trivia Game!</h2>
-			<h2>Score</h2>
-			<button type="button">Add Points</button>
-			<button type="button">Lower Points</button>
-			<div className={'questions-card'}>
+
+			<div className="score">
+				<h2>Score {score}</h2>
+			</div>
+
+			<div className="score-buttons">
+				<button
+					className="btn-increment-points"
+					type="button"
+					onClick={incrementScore}
+				>
+					Add Points
+				</button>
+
+				<button
+					className="btn-decrement-points"
+					type="button"
+					onClick={decrementScore}
+				>
+					Remove Points
+				</button>
+			</div>
+
+			<div className="questions-card">
+				<form onSubmit={props.handleSubmit}>
+					<input
+						type="submit"
+						value="Get New Question"
+						onChange={props.handleChange}
+					></input>
+				</form>
+
 				<div className="category">
-					<h2>CATEGORY: {currentQuestion[0].category.title}</h2>
+					<h2>CATEGORY: {props.currentQuestion[0].category.title}</h2>
 				</div>
-				<h2> QUESTION: {currentQuestion[0].question}</h2>
-				<div className={'hintBox'}>
-					<h2>HINT: {}</h2>
+
+				<div className="question">
+					<h2> QUESTION: {props.currentQuestion[0].question}</h2>
+					<h3> QUESTION VALUE: {props.currentQuestion[0].value}</h3>
 				</div>
-				<div className={'answers'}>
-					<h2>ANSWER: {currentQuestion[0].answer}</h2>
+
+				<div className="answers">
+					<h2>ANSWER: {props.currentQuestion[0].answer}</h2>
 				</div>
 			</div>
 		</div>
